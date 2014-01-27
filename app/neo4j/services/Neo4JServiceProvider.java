8c45a35ae8a1ebd0a9b9b72ca60cd1b406777f33
Johannes Unterstein <unterstein@me.com>
2014-01-27 06:36:49 +0100

    use correct repos

--- app/neo4j/services/Neo4JServiceProvider.java
+++ app/neo4j/services/Neo4JServiceProvider.java
@@ -1,14 +1,20 @@
 package neo4j.services;
 
+import neo4j.repositories.ActorRepository;
+import neo4j.repositories.MovieRepository;
 import neo4jplugin.Neo4JPlugin;
 import neo4jplugin.ServiceProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Neo4JServiceProvider extends ServiceProvider {
 
-//  @Autowired
-//  public NeoUserRepository neoUserRepository;
+  @Autowired
+  public ActorRepository actorRepository;
+
+  @Autowired
+  public MovieRepository movieRepository;
 
   public static Neo4JServiceProvider get() {
     return Neo4JPlugin.get();
