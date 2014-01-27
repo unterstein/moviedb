b9d5fbb6af454b6bc014e28210c17ab87329758a
Johannes Unterstein <unterstein@me.com>
2014-01-27 06:18:32 +0100

    introduced dependencies and configuration

--- project/Build.scala
+++ project/Build.scala
@@ -4,18 +4,21 @@ import play.Project._
 
 object ApplicationBuild extends Build {
 
-  val appName         = "moviedb"
-  val appVersion      = "1.0-SNAPSHOT"
+  val appName = "moviedb"
+  val appVersion = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
     // Add your project dependencies here,
     javaCore,
     javaJdbc,
-    javaEbean
+    javaEbean,
+    "com.github.tuxBurner" %% "play-neo4jplugin" % "1.3.1"
   )
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
+    resolvers += "tuxburner.github.io" at "http://tuxburner.github.io/repo",
+    resolvers += "Neo4j" at "http://m2.neo4j.org/content/repositories/releases/",
+    resolvers += "Spring milestones" at "http://repo.spring.io/milestone"
   )
 
 }
