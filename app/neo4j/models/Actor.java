02bee4dcf849c486eb525c8a53f74f367e6a6588
Johannes Unterstein <j.unterstein@micromata.de>
2014-02-20 17:50:28 +0100

    removed fetch

--- app/neo4j/models/Actor.java
+++ app/neo4j/models/Actor.java
@@ -2,7 +2,6 @@ package neo4j.models;
 
 import neo4j.relations.Relations;
 import org.springframework.data.annotation.TypeAlias;
-import org.springframework.data.neo4j.annotation.Fetch;
 import org.springframework.data.neo4j.annotation.NodeEntity;
 import org.springframework.data.neo4j.annotation.RelatedTo;
 
@@ -14,7 +13,6 @@ public class Actor extends AbstractModel {
 
   public String name;
 
-  @Fetch
   @RelatedTo(type = Relations.ACTOR_MOVIE)
   public Set<Movie> movies;
 }
